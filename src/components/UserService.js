3a0a40a641f9adc34a3b5ba2430431cca2eedf5a
Madhubalajb <jbmadhumng@gmail.com>
2020-02-22 08:49:11 +0530

    User Services added

--- src/components/UserService.js
+++ src/components/UserService.js
@@ -4,6 +4,10 @@ import Login from './Login'
 import Register from './Register'
 
 const UserService = () => {
+    const [loginModal, setLoginModal] = useState(false)
+    const handleModal = () => setLoginModal(true)
+    const handleNoModal = () => setLoginModal(false)
+
     return (
         <div>
             <Button>log In</Button>

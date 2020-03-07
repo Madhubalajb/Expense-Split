f47bc07204bf53504fc2aae1fd3dddeee6bf0b75
Madhubalajb <jbmadhumng@gmail.com>
2020-03-07 11:16:19 +0530

    Display Expenses

--- src/components/DisplayExpense.js
+++ src/components/DisplayExpense.js
@@ -12,18 +12,30 @@ const DisplayExpense = () => {
     const showExpense = () => expenses.map(expense => {
         let date = new Date(expense.date)
         let to_whom = expense.members.filter(member => member.isChecked === true)
-        console.log(to_whom)
         let count = to_whom.length
-        console.log(count)
         let share = expense.amount / count
-        return(
-            <Card key={expense.id}>
+
+        if(count > 0) {
+            return(
+                <Card key={expense.id}>
+                    <h3>Expense Name - {expense.expense_name}</h3>
+                    <p>Amount - {expense.amount}</p>
+                    <p>Date - {`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`}</p>
+                    <p>By -{expense.by_whom}</p>
+                    <p>Members {to_whom.map(member => member.name) } {share}</p>
+                </Card> )
+        }
+        else {
+            return(
+                <Card key={expense.id}>
                 <h3>Expense Name - {expense.expense_name}</h3>
                 <p>Amount - {expense.amount}</p>
                 <p>Date - {`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`}</p>
                 <p>By -{expense.by_whom}</p>
-                <p>Members {to_whom.map(member => member.name)} {share}</p>
-            </Card> )
+                <p>Members {by_whom} {share}</p>
+            </Card> )               
+            )
+        }
         })
    
     return (

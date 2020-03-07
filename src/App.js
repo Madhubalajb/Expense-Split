e58048cb4823b58b2c1d93dced53d86efa7bca05
Madhubalajb <jbmadhumng@gmail.com>
2020-03-07 16:20:33 +0530

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Navigation from './components/Navigation'
+import Home from './components/Home'
+import DisplayExpense from './components/DisplayExpense'
 
 const App = () => {
 
   return (
-      <Navigation />
+      <Router>
+        <Navigation />
+        <Route exact path="/" component={Home} />
+      </Router>
   )
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,31 +1,21 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import {Navbar, Nav} from 'react-bootstrap'
 import UserService from './UserService'
-import Home from './Home'
 
 const Navigation = () => {
     return (
-        <Router>
-            <Navbar collapseOnSelect expand="sm" className="fixed-top">
-                <Navbar.Brand>
-                    <Link to="/"><b>Expense <span className="slash">/</span> Split</b></Link>
-                </Navbar.Brand>
+        <Navbar collapseOnSelect expand="sm" className="fixed-top">
+            <Navbar.Brand href="/">
+                <b>Expense <span className="slash">/</span> Split</b>
+            </Navbar.Brand>
 
-                <Switch>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ml-auto">
-                        <UserService />
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </Router>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ml-auto">
+                    <UserService />
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
 

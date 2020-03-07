ec5a63696b83232ce3763bc5062ee5c918b0780b
Madhubalajb <jbmadhumng@gmail.com>
2020-03-07 16:06:37 +0530

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Navigation from './components/Navigation'
-import Home from './components/Home'
 
 const App = () => {
 
   return (
-    <Router>
       <Navigation />
-      <Route exact path="/" component={Home} />
-    </Router>
   )
 }
 
